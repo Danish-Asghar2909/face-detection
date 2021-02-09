e0415739aae72e4c1057549291e5c4919ac4fdb8
sumbulnaz <sumbul.1304@gmail.com>
2021-02-09 02:04:37 -0800

    Updates

---
+++ precache-manifest.de3b4f219d3426cca79c0e45cca3fac2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a579ce09bde73d0bfe63bff110a514",
+    "url": "/face-detection/index.html"
+  },
+  {
+    "revision": "3c9b80e093bda4b8f5a6",
+    "url": "/face-detection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2a2bc47282fc2badd0f9",
+    "url": "/face-detection/static/css/main.5a168897.chunk.css"
+  },
+  {
+    "revision": "3c9b80e093bda4b8f5a6",
+    "url": "/face-detection/static/js/2.748a36ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-detection/static/js/2.748a36ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2bc47282fc2badd0f9",
+    "url": "/face-detection/static/js/main.640eb584.chunk.js"
+  },
+  {
+    "revision": "7b37c686924301a8fc73",
+    "url": "/face-detection/static/js/runtime-main.7867445b.js"
+  }
+]);
